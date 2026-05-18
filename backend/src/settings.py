29ca214a04333ee47f2961f94b099b2b1125982e
Berton Adiwidya <adiwidya.berton13@gmail.com>
2026-05-18 14:37:31 +0700

    feat(be): add jwt and cors settings with env vars

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from pydantic import BeforeValidator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,6 +10,18 @@ class Settings(BaseSettings):
     )
 
     DB_URI: str
+    JWT_SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_HOURS: int
+    CORS_ORIGINS: Annotated[
+        list[str],
+        BeforeValidator(
+            lambda v: (
+                v
+                if not isinstance(v, str)
+                else [v.strip() for x in v.split(";") if x.strip()]
+            )
+        ),
+    ]
 
 
 settings = Settings()  # pyright: ignore[reportCallIssue]
